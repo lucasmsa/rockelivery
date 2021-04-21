fe5eee481a3b1c901725f090b6cc5c6a4c1a2657
Lucas Moreira <lucasmsea@outlook.com>
2021-04-21 14:07:23 -0300

    chore: add seed for users, items and orders

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,36 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Rockelivery.{User, Item, Order, Repo}
+
+user = %User{
+  age: 19,
+  address: "rua bananona",
+  cep: "58039280",
+  email: "bana@no.com",
+  cpf: "82038003476",
+  name: "Batatao",
+  password: "123456"
+}
+
+%User{id: user_id} = Repo.insert!(user)
+
+item = %Item{
+  category: :drink,
+  description: "suco de banana",
+  photo: "suco_de_banana.png",
+  price: Decimal.new("10.0")
+}
+
+%Item{id: item_id} = Repo.insert!(item)
+
+order = %Order{
+  user_id: user_id,
+  items: [item, item],
+  address: "Rua batatona, 20",
+  payment: :money,
+  comments: "sem banana"
+}
+
+Repo.insert!(order)
